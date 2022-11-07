8cb08499f3d2373088c4a7c803fabf4a29fd9167
Alejandro Perez Gonzalez <ale07pg@gmail.com>
2022-11-07 16:56:17 -0600

    added TODOS

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "ArgumentManager.h"
+#include "queue.h"
 using namespace std;
 
-int main(){
-    cout<<"ddas"<<endl;
+int main(int argc, char *argv[]){
+    /*
+    ArgumentManager am(argc, argv);
+    ifstream input(am.get("input"));
+    ofstream output(am.get("output"));
+    */
+    ifstream input("input1.txt");
+    ofstream output("output1.txt");
+    
+
+    string input_str;
+    //TODO create queue
+
+
+    while (getline(input, input_str)) {
+        if(!input_str.empty())
+        {
+            //TODO check if is valid
+            //TODO infix to postfix
+            //TODO evaluate postfix
+            //TODO add to priority queue depending on the solution of the equation(lower number means higer priority)
+        }        
+    }
+
+    //TODO output queue to file
+
     return 0;
 }
